112b0b4a743730ded94f57a9d9ad5840356b5820
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2026-02-03 18:53:10 +0100

    modif  job1

--- jour2/job01.py
+++ jour2/job01.py
@@ -3,22 +3,28 @@ class Rectangle:
         self.__longueur= a
         self.__largeur= b
     def get_longueur(self):
-        return self.__longueur
+        return f"la longueur est {self.__longueur}"
     def get_largeur(self):
-        return self.__largeur
+        return f"la largeur est {self.__largeur}"
     
     def set__longueur(self,longueur):
-        longueur=int(input())
+        longueur=int(input("longueur: "))
         self.__longueur=longueur
         return self.__longueur
     def set__largeur(self,largeur ):
-        largeur= int(input())
+        largeur= int(input("largeur: "))
         self.__largeur=largeur
         return self.__largeur
 rec=Rectangle(10,5)
 print(rec.get_largeur())
+print(rec.get_longueur())
 rec.set__largeur(5)
+rec.set__longueur(10)
+
 print(rec.get_largeur())
+print(rec.get_longueur())
+
+
 
 
         
