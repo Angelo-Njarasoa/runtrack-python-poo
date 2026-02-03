de18a2ffd113dcf7c0627221120c684b487bff56
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2026-02-03 16:01:39 +0100

    modif job07

---
+++ jour_1/job07.py
@@ -0,0 +1,20 @@
+class Perssonage:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def gauche(self):
+        self.x+=1
+        return self.x
+    def droite(self):
+        self.x-=1
+        return self.x
+    def haut(self):
+        self.y-=1
+        return self.y
+    def bas(self):
+        self.y+=1
+        return self.y
+    def postion(self):
+        return (self.x,self.y)
+pnj=Perssonage(0,0)
+print(pnj.postion())
