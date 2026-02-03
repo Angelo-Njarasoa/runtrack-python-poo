da82d3f9a6fbde2b3a5ae46a728df3d348ed14e2
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2026-02-03 15:43:05 +0100

    modif job06

--- jour_1/job06.py
+++ jour_1/job06.py
@@ -7,9 +7,16 @@ class Animal:
         age_vieillie= self.age +1
         self.age= age_vieillie
         return self.age
+    def nommer(self):
+        self.prenom=(input("nomer l'animal :"))
+        return self.prenom
+
 
 chien=Animal(15,"rex")
 print("l'age de l'animal est ",chien.age,"ans")
 chien.vieillir()
 print("l'age de l'animal est ",chien.age,"ans")
+print("lanimal s'appel",chien.prenom)
+chien.nommer()
+print("l'animal s'appel",chien.prenom)
 
